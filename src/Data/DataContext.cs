a209f11976f8954dfd27467c71374668fc33e732
Alberto Lyons <alberto.lyons@alumnos.ucn.cl>
2025-09-09 21:00:41 -0300

    chore: added Neo4J Driver

---
+++ src/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace perla_metro_route_service.src.Data
+{
+    public class DataContex
+    {
+        
+    }
+}
