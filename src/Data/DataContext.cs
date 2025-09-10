c8d7b3cddb2bb38c47312e4be40ccb48ea6d64af
Alberto Lyons <alberto.lyons@alumnos.ucn.cl>
2025-09-09 21:00:55 -0300

    chore: added Neo4J Driver

--- src/Data/DataContext.cs
+++ src/Data/DataContext.cs
@@ -2,11 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Neo4j.Driver;
 
 namespace perla_metro_route_service.src.Data
 {
-    public class DataContex
+    public class DataContext : IDisposable
     {
+        private readonly IDriver _driver;
+        public DataContext(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+        
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+        
+
         
     }
+    
 }
