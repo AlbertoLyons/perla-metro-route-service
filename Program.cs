42ef9a62678eae8a100b1747819fb81fae532b43
unknown <alberto.lyons@alumnos.ucn.cl>
2025-09-10 21:28:07 -0300

    chore: removed test code in program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,3 @@
-using perla_metro_route_service.src.Data;
-using Route = perla_metro_route_service.src.Models.Route;
-
-DataContext dataContext = new DataContext("neo4j://localhost:7687", "neo4j", "12345678");
-
-Route route = new Route
-{
-    Id = "828",
-    OriginStation = "Station A",
-    DestinationStation = "Station B",
-    DepartureTime = DateTime.UtcNow,
-    ArrivalTime = DateTime.UtcNow.AddMinutes(30),
-    interludeTimes = new List<DateTime> { DateTime.UtcNow.AddMinutes(10), DateTime.UtcNow.AddMinutes(20) },
-    IsActive = true
-};
-
-//await dataContext.CreateRouteAsync(route);
-
-await dataContext.DeleteRouteAsync("828");
 
 var builder = WebApplication.CreateBuilder(args);
 
